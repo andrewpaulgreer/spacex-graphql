14e7ac2b8b868776cf617f6457087c94dc11339b
Andrew Greer <62513150+andrewpaulgreer@users.noreply.github.com>
2020-09-28 16:15:27 -0400

    pushing to heroku

---
+++ public/precache-manifest.be7353fe286a8519809e8ff10b633f0a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4509dd48097719a14a2c007cec022e4f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "efade3db4c4236d34851",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "c6ce433b321608507c38",
+    "url": "/static/js/2.986b70e1.chunk.js"
+  },
+  {
+    "revision": "1168987de8c022b65d0b713d94cc1242",
+    "url": "/static/js/2.986b70e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efade3db4c4236d34851",
+    "url": "/static/js/main.48380b29.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "f2b1b847235119d0ae094a606c74fca4",
+    "url": "/static/media/logo.f2b1b847.jpg"
+  }
+]);
